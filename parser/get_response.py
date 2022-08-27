21acf624688f657427a54984fe23bee84b21dffc
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-08-27 04:02:01 +0300

    bag fix

--- parser/get_response.py
+++ parser/get_response.py
@@ -3,14 +3,15 @@ import asyncio
 from parser.config_parser import URL, HEADERS
 
 
-async def get_response(base_url=URL, link='', headers=HEADERS):
+async def create_session(base_url, link, headers=HEADERS):
     """
     Принимает url главной страницы, url запрашиваемой страницы и UserAgent
+    Создаёт web-сессию
     Возвращает ответ на get-запрос к странице
     :param link: str
     :param base_url: str
     :param headers: dict
-    :return: html text
+    :return: aiohttp.client_reqrep.ClientResponse: response to a request
     """
     url = base_url + link
     async with aiohttp.ClientSession() as session:
@@ -19,11 +20,18 @@ async def get_response(base_url=URL, link='', headers=HEADERS):
             return resp
 
 
-async def main():
-    task = asyncio.create_task(get_response())
+async def get_response(base_url=URL, link=''):
+    """
+    Создаёт задачу из корутины create_session
+    Возвращает результат
+    :param base_url: str
+    :param link: str
+    :return: aiohttp.client_reqrep.ClientResponse: response to a request
+    """
+    task = asyncio.create_task(create_session(base_url=base_url, link=link))
     result = await task
-    print(result.status)
+    return result
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    print(asyncio.run(get_response()).status)
