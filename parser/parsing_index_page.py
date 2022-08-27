da4b16dc9fd28b3564cf71b8eb2045ae7106fcf3
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-08-27 07:56:51 +0300

    parsing_index_page.py is ready

---
+++ parser/parsing_index_page.py
@@ -0,0 +1,36 @@
+import re
+
+from bs4 import BeautifulSoup
+from get_response import get_response
+import asyncio
+
+
+def get_links(html_code=asyncio.run(get_response())):
+    """
+    Получает html код главной страницы
+    Извлекает из него ссылки на страницы категорий товаров
+    :param html_code: str
+    :yield: {title: str: url: str}:
+    """
+    soup = BeautifulSoup(html_code, features='lxml')
+    list_menu = soup.find_all('li', class_='level_1')
+    tuple_links = (
+        elem.find('a', href=re.compile('/catalog/'), class_=False, text=True) for elem in list_menu
+    )
+    my_exceptions = ('Тематические подборки', 'Акции')
+    for link in tuple_links:
+        try:
+            if link.text not in my_exceptions:
+                yield {link.text: link.get('href')}
+        except AttributeError:
+            continue
+
+
+def main():
+    result = get_links()
+    for res in result:
+        print(res)
+
+
+if __name__ == '__main__':
+    main()
