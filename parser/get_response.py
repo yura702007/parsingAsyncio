480ae0636e5202566ad5854e76e5b3de9a3798fb
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-08-28 13:03:05 +0300

    add async def main()

--- parser/get_response.py
+++ parser/get_response.py
@@ -1,38 +1,45 @@
 import aiohttp
 import asyncio
-from parser.config_parser import URL, HEADERS
+from config_parser import URL, HEADERS
 
 
-async def create_session(base_url, link, headers=HEADERS):
+async def create_session(base_url, headers=HEADERS):
     """
-    Принимает url главной страницы, url запрашиваемой страницы и UserAgent
+    Принимает url страницы и UserAgent
     Создаёт web-сессию
-    Возвращает ответ на get-запрос к странице
-    :param link: str
+    Возвращает текст ответа на get-запрос к странице
     :param base_url: str
     :param headers: dict
     :return: aiohttp.client_reqrep.ClientResponse: response to a request
     """
-    url = base_url + link
     async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=headers) as resp:
+        await asyncio.sleep(1)
+        async with session.get(url=base_url, headers=headers) as resp:
             assert resp.status < 400
-            result = await resp.text()
-            return result
+            return await resp.text()
 
 
-async def get_response(base_url=URL, link=''):
+async def get_response(base_url=URL, links=None):
     """
     Создаёт задачу из корутины create_session
     Возвращает результат
     :param base_url: str
-    :param link: str
+    :param links: generator
     :return: aiohttp.client_reqrep.ClientResponse: response to a request
     """
-    task = asyncio.create_task(create_session(base_url=base_url, link=link))
+    if links:
+        tasks = (asyncio.create_task(create_session(list(link.values())[0])) for link in links)
+        result = await asyncio.gather(*tasks, return_exceptions=True)
+        return result
+    task = asyncio.create_task(create_session(base_url=base_url))
     result = await task
     return result
 
 
+async def main():
+    result = await get_response()
+    print(result)
+
+
 if __name__ == '__main__':
-    print(asyncio.run(get_response()))
+    asyncio.run(main())
