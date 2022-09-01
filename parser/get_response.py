f72f1343739f45cf176cf4b38ddf463ee3dd755c
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-09-01 15:52:59 +0300

    get_response.py is ready

--- parser/get_response.py
+++ parser/get_response.py
@@ -1,57 +1,20 @@
-from pprint import pprint
-
-import aiohttp
+from aiohttp import ClientSession
 import asyncio
 from config_parser import URL, HEADERS
-from parser_index_page import get_links
-from product_page_parser import parser_page
 
 
-async def create_session(base_url, header=None, headers=HEADERS):
-    """
-    Принимает url страницы и UserAgent
-    Создаёт web-сессию
-    Возвращает текст ответа на get-запрос к странице
-    :param base_url: str
-    :param header: str
-    :param headers: dict
-    :return: aiohttp.client_reqrep.ClientResponse: response to a request
-    """
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=base_url, headers=headers) as resp:
-            while True:
-                assert resp.status < 400
-                if header:
-                    return {header: await resp.text()}
-                return await resp.text()
+async def create_session(url):
+    async with ClientSession() as sessions:
+        async with sessions.get(url=url, headers=HEADERS) as resp:
+            assert resp.status == 200
+            return await resp.text()
 
 
-async def get_response(base_url=URL, links=None):
-    """
-    Создаёт задачу из корутины create_session
-    Возвращает результат
-    :param base_url: str
-    :param links: generator
-    :return: aiohttp.client_reqrep.ClientResponse: response to a request
-    """
-    if links:
-        tasks = []
-        for header, link in links:
-            task = asyncio.create_task(create_session(header=header, base_url=link))
-            tasks.append(task)
-        result = await asyncio.gather(*tasks, return_exceptions=True)
-        return result
-    task = asyncio.create_task(create_session(base_url=base_url))
-    result = await task
+async def get_response(url=URL):
+    task = asyncio.create_task(create_session(url=url))
+    result = await asyncio.gather(task)
     return result
 
 
-async def main():
-    html_index_page = await get_response()
-    links = get_links(html_index_page)
-    html_product_pages = await get_response(links=links)
-    pprint(html_product_pages[0])
-
-
 if __name__ == '__main__':
-    asyncio.run(main())
+    print(asyncio.run(get_response()))
