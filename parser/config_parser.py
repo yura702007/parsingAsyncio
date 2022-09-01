b209251c1594c77e33d2dcd158abd14eecbf9caa
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-09-01 20:20:15 +0300

    config_parser.py is ready

--- parser/config_parser.py
+++ parser/config_parser.py
@@ -6,7 +6,6 @@ HEADERS = {
     "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
     "X-Amzn-Trace-Id": "Root=1-630084e5-1013eda313f8fa782be2c3b0"
-  }
+}
 
 TITLES = ['title', 'country', 'price', 'url']
-
