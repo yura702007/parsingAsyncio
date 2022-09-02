b4a692d02f543b67f58d728a28c872d4833a717b
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-09-02 12:54:23 +0300

    add next step to parse_html method

--- parser/parser.py
+++ parser/parser.py
@@ -1,8 +1,10 @@
 import asyncio
+import time
 from pprint import pprint
 
 from bs4 import BeautifulSoup
 from get_response import create_session
+from config_parser import URL, TITLES
 
 
 class Parser:
@@ -22,17 +24,28 @@ class Parser:
         cards_div = soup.find('div', class_='products_block__wrapper products_4_columns vertical')
         cards = cards_div.find_all('div', class_='form_wrapper')
         for card in cards:
-            title = card.find('div', class_='title').text.strip()
-            url = card.find('a', class_='fancy_ajax').get('href')
-            price = ''.join(card.find('div', class_='price').text.strip().split())
-            country = card.find('div', class_='small_country').text.strip()
-            data.append({'title': title, 'url': url, 'price': price, 'country': country})
+            _dict = {'title': None, 'url': None, 'price': None, 'country': None}
+            try:
+                _dict['title'] = card.find('div', class_='title').text.strip()
+                _dict['url'] = card.find('a', class_='fancy_ajax').get('href')
+                _dict['price'] = ''.join(card.find('div', class_='price').text.strip().split())
+                _dict['country'] = card.find('div', class_='small_country').text.strip()
+            except AttributeError:
+                pass
+            finally:
+                data.append(_dict)
+        show_more = cards_div.find('a', class_='show_more')
+        if show_more:
+            self.url = URL + show_more.get('href')
+        else:
+            self.url = False
         return data
 
     async def run(self):
-        page = await self.get_html()
-        result = await self.parse_html(page)
-        pprint(result)
+        result = []
+        while self.url:
+            page = await self.get_html()
+            result.extend(await self.parse_html(page))
 
 
 if __name__ == '__main__':
@@ -40,9 +53,12 @@ if __name__ == '__main__':
 
     # for _title, _url in links:
     #     print(Parser(url=_url, title=_title))
+    print('start')
+    start = time.time()
     _title, _url = next(links)
     p = Parser(title=_title, url=_url)
     asyncio.run(p.run())
+    print(time.time() - start)
 
 """
 <a class="show_more" href="/catalog/8011.html?lazy_steep=2">
