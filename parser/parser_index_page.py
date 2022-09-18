55f7778125e9181fddedf427acaad5a3110012d5
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-09-18 03:00:26 +0300

    add logger

--- parser/parser_index_page.py
+++ parser/parser_index_page.py
@@ -1,4 +1,6 @@
 import asyncio
+import logging
+import logging.config
 from datetime import date
 from pprint import pprint
 
@@ -7,9 +9,12 @@ from aiohttp import ClientSession
 from pathlib import Path
 from config_parser import HEADERS
 
+logging.config.fileConfig('logging.conf')
+
 
 class Parser:
     headers = HEADERS
+    my_exception = ('Уцененные товары', 'Карты лояльности, сувениры', 'Шеф-онлайн')
 
     def __init__(self, url='https://e-dostavka.by/catalog/'):
         self.url = url
@@ -25,16 +30,19 @@ class Parser:
         try:
             self.path = Path('..', 'data', f'{date.today()}')
             self.path.mkdir(parents=True)
+            logging.debug('dir is create')
         except FileExistsError:
             pass
 
     async def create_session(self):
         async with ClientSession() as session:
             async with session.get(url=self.url, headers=self.headers) as resp:
+                logging.debug(f'{resp.url}, {resp.status}')
                 try:
                     assert resp.status == 200
                     return await resp.text()
-                except AssertionError:
+                except AssertionError as exc:
+                    logging.error(exc, exc_info=True)
                     return
 
     async def pars_page(self):
@@ -42,19 +50,19 @@ class Parser:
         soup = BeautifulSoup(_html, 'lxml')
         block = soup.find('div', class_='rubrics_table clearfix')
         catalog = block.find_all('div')
-        my_exception = ('Уцененные товары', 'Карты лояльности, сувениры', 'Шеф-онлайн')
         category_dict = {}
         for category in catalog:
             links = []
             rubrics = category.find_all('div', class_='item')
             if rubrics:
-                if category.find('div', class_='title').text in my_exception:
+                if category.find('div', class_='title').text in self.my_exception:
                     continue
                 category_name = category.find('div', class_='title').text
                 for rubric in rubrics:
                     a = rubric.find('a')
                     links.append((a.get('href'), a.text))
                 category_dict[category_name] = links
+        logging.debug('urls_list is ready')
         return category_dict
 
 
